c89417f112fdaa74401bfc07f8ff4d0af8b2a26c
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2021-02-07 04:03:21 +0800

    Add linting

---
+++ src/components/main.tsx
@@ -0,0 +1,10 @@
+import Grid from '@material-ui/core/Grid'
+import React from 'react'
+
+export const Main: React.FC = () => {
+    return (
+        <Grid>
+            This
+        </Grid>
+    )
+}
