02415f798b06e386b4a194d0418a2f117503173a
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2021-02-08 00:30:21 +0800

    Typing check

---
+++ src/components/View.tsx
@@ -0,0 +1,39 @@
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import clsx from 'clsx'
+import React from 'react'
+import { Character } from './Main'
+
+type ViewProps = {
+    characters: Character[]
+}
+
+const useStyles = makeStyles((theme) => ({
+    correct: {
+        color: 'green',
+    },
+    wrong: {
+        color: 'red',
+    },
+}))
+
+export const View: React.FC<ViewProps> = ({ characters }) => {
+    const classes = useStyles()
+
+    return (
+        <>
+            {characters.map((value, index) => (
+                <Typography
+                    key={index}
+                    component="span"
+                    className={clsx(
+                        value.display === 'wrong' && classes.wrong,
+                        value.display === 'correct' && classes.correct,
+                    )}
+                >
+                    {value.value}
+                </Typography>
+            ))}
+        </>
+    )
+}
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -10,6 +10,12 @@ import Typography from '@material-ui/core/Typography'
 import MenuIcon from '@material-ui/icons/Menu'
 import { LoremIpsum } from 'lorem-ipsum'
 import React from 'react'
+import { View } from './View'
+
+export interface Character {
+    value: string
+    display: 'initial' | 'correct' | 'wrong'
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,51 +27,163 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    correct: {
+        color: 'green',
+    },
+    wrong: {
+        color: 'red',
+    },
 }))
 
 const lorem = new LoremIpsum({
     sentencesPerParagraph: {
-        max: 8,
-        min: 4,
+        max: 7,
+        min: 6,
     },
     wordsPerSentence: {
-        max: 16,
-        min: 4,
+        max: 7,
+        min: 6,
     },
 })
 
+const debounce = (func: any, delay: number) => {
+    let timerId: NodeJS.Timeout
+    return (...args: any) => {
+        clearTimeout(timerId)
+        timerId = setTimeout(() => func(...args), delay)
+    }
+}
+
 export const Main: React.FC = () => {
     const classes = useStyles()
-    const words: string[] = lorem.generateParagraphs(7).split(' ')
+    const [characters, setCharacters] = React.useState<Character[]>([])
+    const [typedCharacters, setTypedCharacters] = React.useState<string>('')
+    const [inputValue, setInputValue] = React.useState<string>('')
+
+    const [time, setTime] = React.useState<number>(180)
+
+    console.log('inputValue', inputValue)
+    const handleChange = React.useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const input = event.currentTarget.value
+            const val = typedCharacters + input
+            const currentChar = val.length - 1
+
+            // check for backspace action which empties input
+            if (input.length === 0 && typedCharacters.length > 0 && inputValue.length === 0) {
+                return
+            }
+
+            let newCharacters = [...characters]
+            let hasWrong = characters.slice(0, currentChar).some((each) => each.display === 'wrong')
+
+            // at the start of paragraph
+            if (currentChar < 0) {
+                newCharacters = characters.map((each) => {
+                    return {
+                        ...each,
+                        display: 'initial',
+                    }
+                })
+            } else {
+                if (val[currentChar] !== characters[currentChar].value || hasWrong) {
+                    hasWrong = true
+                    newCharacters = characters.map((each, index) => {
+                        if (currentChar <= index) {
+                            return {
+                                ...each,
+                                display: 'wrong',
+                            }
+                        }
+                        return each
+                    })
+                } else {
+                    newCharacters = characters.map((each, index) => {
+                        if (currentChar >= index) {
+                            return {
+                                ...each,
+                                display: 'correct',
+                            }
+                        }
+                        return {
+                            ...each,
+                            display: 'initial',
+                        }
+                    })
+                }
+            }
+
+            setCharacters(newCharacters)
+            if (input.includes(' ') && !hasWrong) {
+                setInputValue('')
+                setTypedCharacters(val)
+            } else {
+                setInputValue(input)
+            }
+        },
+        [characters, typedCharacters, inputValue],
+    )
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        console.log('keydown', event.key)
+    }
+
+    React.useEffect(() => {
+        if (time !== 0) {
+            const timer = setTimeout(() => {
+                console.log('timeout')
+                setTime(time - 1)
+            }, 1000)
+            return () => clearTimeout(timer)
+        }
+    })
+
+    React.useEffect(() => {
+        if (Object.keys(characters).length === 0) {
+            const paragraph = lorem.generateParagraphs(1)
+            const characters: Character[] = Array.from(paragraph).map((each) => ({
+                value: each,
+                display: 'initial',
+            }))
+
+            setCharacters(characters)
+        }
+    }, [])
 
     return (
         <Container>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        News
+                        Words Per Minute
+                    </Typography>
+                    <Typography variant="h6" className={classes.title}>
+                        TIme: {time}
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
-            <Grid>
+            <Grid container spacing={2}>
                 <Grid item>
-                    <TextField
-                        fullWidth
-                        id="filled-multiline-static"
-                        label="Words"
-                        multiline
-                        rows={10}
-                        defaultValue={words.join(' ')}
-                        variant="filled"
-                        contentEditable={false}
-                    />
+                    <View characters={characters} />
                 </Grid>
                 <Grid item>
-                    <TextField label="Word" id="wordInput" variant="filled" />
+                    <TextField
+                        value={inputValue}
+                        label="Word"
+                        id="wordInput"
+                        variant="outlined"
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}
+                    />
                 </Grid>
             </Grid>
         </Container>
